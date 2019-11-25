26e9d177e2912f2f6b377ec9135764bf1e554b28
EkuDS1 <30476238+EkuDS1@users.noreply.github.com>
2019-11-25 16:42:45 +0500

    formatted (for previous commit)

--- Indexer.java
+++ Indexer.java
@@ -8,29 +8,27 @@ public class Indexer {
 	public static void main(String[] args) throws IOException {
 		// map key is imdb_id and map value is overview
 		HashMap<String, String> map = new HashMap<>();
-		
+
 		String parsingString;
-		
+
 		File csvData = new File("data/movies_metadata.csv");
-		
+
 		// this parser will go through record-by-record in the csv
 		CSVParser parser = CSVParser.parse(csvData, Charset.forName("UTF-8"), CSVFormat.EXCEL);
-		
-		
+
+
 		// this should add all records to the hashmap
 		for (CSVRecord currentRecord : parser) {
-			
+
 			parsingString = (currentRecord.get(7) + " " + currentRecord.get(1)).toLowerCase();
-			
+
 			map.put(currentRecord.get(0), Parser.parseWords(parsingString));
 		}
-	
-		
-		
+
 		for(Map.Entry<String, String> entry : map.entrySet()) {
 			System.out.println(entry.getKey() + ":" + entry.getValue());
 		}
-		
+
 		System.out.println("\nSize: " + map.size());
 
 	}
